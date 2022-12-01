6ebc4d68746b332ddc1cf3e94e4bb7e159b43c06
ElusiveX <keegansmccorkle03@gmail.com>
2022-11-30 20:37:27 -0700

    Added example 3

---
+++ day 1/example3.js
@@ -0,0 +1,3 @@
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+let name = 'keegan', job = 'Bus Driver', live = 'United States'
+console.log(name, job, live)
