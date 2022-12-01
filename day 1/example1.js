2ebdcf3fd172abfa0a16462a8cf9cf5dbe3af7a6
ElusiveX <keegansmccorkle03@gmail.com>
2022-11-30 20:31:30 -0700

    Update example1.js

---
+++ day 1/example1.js
@@ -0,0 +1,8 @@
+let firstName = 'Keegan' // first name
+let lastName = 'McCorkle' //last name
+let country = 'United States Of America'  // country
+let city = 'Denver' //city
+let age = 19 // age
+let isMarried = true // true or false value.
+
+console.log(firstName, lastName, country, city, age, isMarried)
