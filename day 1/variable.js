066abbda2e0f94135e47b161203f231c990f2cd9
ElusiveX <keegansmccorkle03@gmail.com>
2022-11-30 20:47:11 -0700

    testing my dumb

---
+++ day 1/variable.js
@@ -0,0 +1,24 @@
+//single line comment comments can make code more readable
+//welcome to 30days
+
+/*
+ comments can make code easier
+ Easy to reuse and infomative
+ */
+ let firstName1 = 'Keegan' // first name
+ let lastName1 = 'McCorkle' //last name
+ let country1 = 'United States Of America'  // country
+ let city1 = 'Denver' //city
+ let age3 = 19 // age
+ let isMarried1 = true // true or false value.
+
+ let bs1 
+ let bs2 
+ let bs3
+ let bs4
+
+
+ let myAge = 19
+ let hisAge = 30
+ console.log(`I am`, myAge, 'years old. He is', hisAge, 'years old.')
+ console.log(bs1,bs2,bs3,bs4)
