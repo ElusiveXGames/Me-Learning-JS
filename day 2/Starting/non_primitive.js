e5d76545c5072d02718f2794b41450f5014ac204
ElusiveX <keegansmccorkle03@gmail.com>
2022-12-01 16:08:36 -0700

    numbers

--- day 2/Starting/non_primitive.js
+++ day 2/Starting/non_primitive.js
@@ -1,1 +1,21 @@
 let nums = [1, 2, 3]
+nums[0] = 10
+console.log(nums)
+
+
+let nums1 = [1, 2, 3]
+let numbers1 = [1, 2, 3]
+console.log(nums1 == numbers1)
+
+let userOne = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+let userTwo = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+
+console.log(userOne == userTwo)
