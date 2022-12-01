02220850faa6639c6adac4576f49d7e66e0e802c
ElusiveX <keegansmccorkle03@gmail.com>
2022-11-30 20:34:21 -0700

    example 2
    Example 2 and updarte index to show the js

---
+++ day 1/example2.js
@@ -0,0 +1,8 @@
+//varriables with Numbers
+
+let age = 19 // age in years
+let gravity = 9.81 //earth gravity
+let boilingPoint = 100 //degrees in *c
+const PI = 3.14 //pi
+
+console.log(age, gravity, boilingPoint, PI)
